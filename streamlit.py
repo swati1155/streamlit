11fa234b684761154e8a6e846f0ddad916fdaec0
juju1155 <swati1155@gmail.com>
2025-02-09 03:06:05 +0400

    remove volume function

--- streamlit.py
+++ streamlit.py
@@ -2,15 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import webbrowser
-import osascript
+# import osascript
 import time
+# import applescript
 
-# st.link_button("Go to gallery", "https://streamlit.io/gallery")
-
-button = st.button('View')
+button = st.button('Click Here')
 
 if button:
-    osascript.osascript("set volume output volume 10")
-    for i in range(10):
+    # osascript.osascript("set volume output volume 100")
+    for i in range(15):
         webbrowser.open_new_tab('https://www.youtube.com/shorts/wIsQpQ8fLSs')
         time.sleep(2)
