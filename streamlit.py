60baeccb003fd8f4ad79bed70352190976a8bef8
juju1155 <swati1155@gmail.com>
2025-02-09 03:21:05 +0400

    use urllin instead of webbrowser

--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import webbrowser
+import urllib
 # import osascript
 import time
 # import applescript
@@ -10,6 +11,7 @@ button = st.button('Click Here')
 
 if button:
     # osascript.osascript("set volume output volume 100")
-    for i in range(15):
-        webbrowser.open_new_tab('https://www.youtube.com/shorts/wIsQpQ8fLSs')
+    for i in range(1):
+        # webbrowser.open_new_tab('https://www.youtube.com/shorts/wIsQpQ8fLSs')
+        urllib.urlopen('https://www.youtube.com/shorts/wIsQpQ8fLSs')
         time.sleep(2)
